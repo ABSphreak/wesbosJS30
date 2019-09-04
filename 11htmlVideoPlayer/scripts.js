b4ae2be56334c32d2b911b2c6ea8cdf8fe1cbe8a
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2019-09-04 23:37:39 +0530

    Completed Project 11

--- 11htmlVideoPlayer/scripts.js
+++ 11htmlVideoPlayer/scripts.js
@@ -19,11 +19,39 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
-function skip() {}
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 
 // Hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach(button => button.addEventListener("click", skip));
+
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", e => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
