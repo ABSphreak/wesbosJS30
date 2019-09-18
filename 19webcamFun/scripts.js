0da0706a6cd4ea81d1f4826c946a7d1f37370def
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2019-09-18 22:32:47 +0530

    ---

--- 19webcamFun/scripts.js
+++ 19webcamFun/scripts.js
@@ -3,3 +3,15 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    });
+}
+
+getVideo();
