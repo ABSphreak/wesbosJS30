b8e8cfe2578e55e2afe1ac9fcfb2e527c620677d
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2019-10-10 22:52:33 +0530

    Created One effect

--- 19webcamFun/scripts.js
+++ 19webcamFun/scripts.js
@@ -9,7 +9,9 @@ function getVideo() {
     .getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
-      video.src = window.URL.createObjectURL(localMediaStream);
+      // video.src = window.URL.createObjectURL(localMediaStream);
+      // video.play();
+      video.srcObject = localMediaStream;
       video.play();
     })
     .catch(err => {
@@ -22,6 +24,39 @@ function paintToCanvas() {
   const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  // play the sound of clicking a picture
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] + 0.5; //BLUE
+  }
+  return pixels;
 }
 
 getVideo();
+
+video.addEventListener("canPlay", paintToCanvas);
