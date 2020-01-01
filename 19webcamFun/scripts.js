2bcd168d14ba3e8a4682e808d9ee7a6f8c2cd423
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-01-01 14:07:32 +0530

    Completed Project 19 by cheating

--- 19webcamFun/scripts.js
+++ 19webcamFun/scripts.js
@@ -9,13 +9,19 @@ function getVideo() {
     .getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
-      // video.src = window.URL.createObjectURL(localMediaStream);
-      // video.play();
+
+      //  DEPRECIATION :
+      //       The following has been depreceated by major browsers as of Chrome and Firefox.
+      //       video.src = window.URL.createObjectURL(localMediaStream);
+      //       Please refer to these:
+      //       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+      //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
       video.srcObject = localMediaStream;
       video.play();
     })
     .catch(err => {
-      console.error(`Oh No!!`, err);
+      console.error(`OH NO!!!`, err);
     });
 }
 
@@ -27,15 +33,22 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-
+    // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
-    pixels = redEffect(pixels);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
 function takePhoto() {
-  // play the sound of clicking a picture
+  // played the sound
   snap.currentTime = 0;
   snap.play();
 
@@ -50,14 +63,51 @@ function takePhoto() {
 
 function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 100; //RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] + 0.5; //BLUE
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
 }
 
-// Start the Video Camera
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach(input => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
 getVideo();
 
-video.addEventListener("canPlay", paintToCanvas);
+video.addEventListener("canplay", paintToCanvas);
